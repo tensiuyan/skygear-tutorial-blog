b5f0428bfe940b1908215dfbc0feee845ee0a166
tensiuyan <tentang@oursky.com>
2018-01-19 16:19:11 +0800

    save comments

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const showContent = () => {
       <h2>${blogPost.title}</h2>
       <p>${blogPost.createdAt}</p>
       <p style='margin-top:20px;'>${blogPost.content}</p>
-      <form class="form-comment" id='${blogPost.id}' />
+      <form class="form-comment" id='${blogPost.id}' onSubmit='submitComment(event)' />
         <input class="comment-text" type='text' placeholder="input comment here"/>
         <input class="comment-submit" type='submit' value="comment" />
       </form>
@@ -84,55 +84,47 @@ const showContent = () => {
   ).then((elements) => {
     document.getElementById("content").innerHTML = elements.join(' ');
   });
-
-  // const BlogPost = skygear.Record.extend("blogpost");
-  // const queryBlogPost = new skygear.Query(BlogPost);
-  // queryBlogPost.addDescending('_created_at');
-  //
-  // skygear.publicDB.query(queryBlogPost).then((blogpost) => {
-  //
-  //   const blogID = (blogpost) => {
-  //     return blogpost.id;
-  //   }
-  //
-  //   const list = blogpost.map((blogpost) => {
-  //     return `
-  //       <article style='margin-top:40px;'>
-  //         <h2>${blogpost.title}</h2>
-  //         <p>${blogpost.createdAt}</p>
-  //         <p style='margin-top:20px;'>${blogpost.content}</p>
-  //         <form class="form-comment" id='${blogID(blogpost)}' />
-  //           <input class="comment-text" type='text' placeholder="input comment here"/>
-  //           <input class="comment-submit" type='submit' value="comment" />
-  //         </form>
-  //       </article>
-  //     `
-  //   }).join('');
-  //   document.getElementById("content").innerHTML = list;
-  // }).then(() => {
-  //   submitComment();
-  // });
 }
 
-const submitComment = () => {
-  const forms = document.getElementsByClassName("form-comment");
 
-  for (var i=0; i<forms.length; i++){
-    forms.item(i).addEventListener("submit", (e) => {
-      e.preventDefault();
-      console.log(e);
+const submitComment = (e) => {
+    e.preventDefault();
+    console.log(e);
 
-      const BlogPost = skygear.Record.extend("blogpost");
-      const blogpost = new BlogPost ({
-        "id": e.target.id,
-      });
+    const BlogPost = skygear.Record.extend("blogpost");
+    const blogpost = new BlogPost ({
+      "id": e.target.id,
+    });
 
-      const commentInput = e.target[0].value;
-      const Comment = skygear.Record.extend("comment");
-      const comment = new Comment ({
-        "comment": commentInput,
-        "blogPost": new skygear.Reference(blogpost)
-      });
-    })
-  }
+    const commentInput = e.target[0].value;
+    const Comment = skygear.Record.extend("comment");
+    const comment = new Comment ({
+      "comment": commentInput,
+      "blogPost": new skygear.Reference(blogpost)
+    });
+    skygear.publicDB.save(comment).then(showContent);
 }
+
+
+// const submitComment = () => {
+//   const forms = document.getElementsByClassName("form-comment");
+//
+//   for (var i=0; i<forms.length; i++){
+//     forms.item(i).addEventListener("submit", (e) => {
+//       e.preventDefault();
+//       console.log(e);
+//
+//       const BlogPost = skygear.Record.extend("blogpost");
+//       const blogpost = new BlogPost ({
+//         "id": e.target.id,
+//       });
+//
+//       const commentInput = e.target[0].value;
+//       const Comment = skygear.Record.extend("comment");
+//       const comment = new Comment ({
+//         "comment": commentInput,
+//         "blogPost": new skygear.Reference(blogpost)
+//       });
+//     })
+//   }
+// }
